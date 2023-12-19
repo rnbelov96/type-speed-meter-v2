faa13283a8f2e38fbbdf394783f51a9bd863daab
Belov Roman <rnbelov@southmedia.ru>
2023-12-19 22:26:53 +0300

    Удаление лишнего кода

--- frontend/js/utils/create-hidden-input.js
+++
@@ -1,9 +0,0 @@
-export default ({ name, value }) => {
-  const hiddenInputEl = document.createElement('input');
-  hiddenInputEl.setAttribute('type', 'text');
-  hiddenInputEl.setAttribute('hidden', true);
-  hiddenInputEl.setAttribute('name', name);
-  hiddenInputEl.value = value;
-
-  return hiddenInputEl;
-};
